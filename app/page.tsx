a1b847b4e11e2e159376c467d992930c9caa2176
nguyen89mke <nguyen89.mke@gmail.com>
2025-04-13 16:51:48 -0500

    Created HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+import { events } from "@/lib/data";
+import { EventCard } from "@/components/EventCard";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div className={styles.page}></div>;
+  return (
+    <main className={styles.eventsMain}>
+      <div className={styles.eventsGrid}>
+        {events.map((event) => (
+          <EventCard key={event.id} event={event} />
+        ))}
+      </div>
+    </main>
+  );
 }

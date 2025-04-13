55d1e0e805316a836d9c45fc1596680d008f0253
nguyen89mke <nguyen89.mke@gmail.com>
2025-04-13 17:10:43 -0500

    Fixed Navbar. Must click title to go to homepage instead of anywhere on nav

--- components/NavigationBar/index.tsx
+++ components/NavigationBar/index.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
 import { Trophy } from "lucide-react";
 import styles from "@/components/NavigationBar/styles.module.css";
 
 const NavigationBar = () => {
   return (
     <nav className={styles.navContainer}>
-      <Trophy />
-      <h1>Bracket Buddy</h1>
+      <Link href="/">
+        <Trophy />
+        <h1>Bracket Buddy</h1>
+      </Link>
     </nav>
   );
 };
